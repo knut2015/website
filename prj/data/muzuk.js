bd69b75a7190a609f4fe2fe1c55e3330e80be884
knut2015 <muzuk99@naver.com>
2015-03-12 15:49:59 +0900

    js

--- prj/data/muzuk.js
+++ prj/data/muzuk.js
@@ -117,7 +117,7 @@ var Works = Works || function(){
 				for ( i = 0; i < jsonResultModule.getJsonData()[0].work[config.idx].imgs.length; i++){
 					
 					var imgs = jsonResultModule.getJsonData()[0].work[config.idx].imgs[i].viewImg;
-					$(".detailImages").append("<p><img src='"+imgs+"'></p>");
+					$(".detailImages").append("<p><img src='"+imgs+"'></p>").fadeIn();
 				}
 
 				workTable.fadeOut();
@@ -131,7 +131,9 @@ var Works = Works || function(){
 			});
 		},
 		detailImageInit: function(){
-			$(".detailImages").empty();
+			$(".detailImages").fadeOut(300, function(){
+				$(this).empty();
+			});
 		}
 	}
 
@@ -154,16 +156,20 @@ var Works = Works || function(){
 
 	function onClickDetailView(event){
 		prj.detailImageInit();
-		
+
 		switch (event.currentTarget.className){
 			case "btnPrev":
 				index--;
-				if ( index < 0 ) index = 0;
+				if ( index < 0 ) {
+					// index = 0;
+					return;
+				}
 				break;
 			case "btnNext":
 				index++;
 				if ( index > jsonResultModule.getJsonData()[0].work.length -1 ){
-					index = jsonResultModule.getJsonData()[0].work.length -1;
+					// index = jsonResultModule.getJsonData()[0].work.length -1;
+					return;
 				}
 				break;
 		}
