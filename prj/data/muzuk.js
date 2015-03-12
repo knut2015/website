4f3062a43c836bc749cd8d4ee1f95f62c0471130
knut15 <skymuzuk@gmail.com>
2015-03-12 20:46:29 +0900

    js

--- prj/data/muzuk.js
+++ prj/data/muzuk.js
@@ -194,7 +194,7 @@ var Photos = Photos || function(){
 					$(this).css('overflow-y', 'auto');
 				});
 			}else if ( state == "end" ){
-				photoView.css('overflow-y', 'none').stop().animate({left: '-100%'}, 400, function(){
+				photoView.css('overflow', 'none').stop().animate({left: '-100%'}, 400, function(){
 					photo.closePhoto();
 					$(this).css('display', 'none');
 				});
