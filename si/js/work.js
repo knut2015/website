bf7633d54edd5e43dde99411f734f6c1536d7ab9
knut15 <skymuzuk@gmail.com>
2015-04-02 14:37:44 +0900

    si

--- si/js/work.js
+++ si/js/work.js
@@ -97,8 +97,9 @@ function getItemElement(cal) {
 		var widthClass = wRand > 0.85 ? 'width3' : wRand > 0.7 ? 'width2' : '';
 		var heightClass = hRand > 0.85 ? 'height3' : hRand > 0.5 ? 'height2' : '';
 		// $item.addClass( widthClass ).addClass( heightClass );
-		return $item;
+		
 	});
+	return $item;
 }
 
 
