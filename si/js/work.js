b647d2bff34bd6a4fff89184e94a856d41f9905b
knut15 <skymuzuk@gmail.com>
2015-04-03 23:40:07 +0900

    si

--- si/js/work.js
+++ si/js/work.js
@@ -91,10 +91,10 @@ var CreateWork = CreateWork || function(){
 			$(".detailView").css({"top": currentThumbY + "px", "display":"block", "height":"1000px"});
 
 			$(".item").each(function(){
-				console.log($(this).position().top +", "+ currentThumbY);
-				// if ( $(this).position().top > currentThumbY){
-					// console.log($(".item").position().top);
-				// }
+				// console.log($(this).position().top +", "+ currentThumbY);
+				if ( $(this).position().top > currentThumbY){
+					console.log($(".item").position().top);
+				}
 			});
 		});
 	}
